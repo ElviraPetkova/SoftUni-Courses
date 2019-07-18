cd5d25aa1a5a901d491848d7e335aca948004759
Elvira <elvira.lyubomirova.petkova@abv.bg>
2019-07-18 11:27:11 +0300

    Change one solution

--- C#Advanced - 2019/3. Sets and Dictionaries Advanced - Exercise/07. The V-Logger/Program.cs
+++ C#Advanced - 2019/3. Sets and Dictionaries Advanced - Exercise/07. The V-Logger/Program.cs
@@ -8,7 +8,75 @@ namespace _07._The_V_Logger
     {
         static void Main(string[] args)
         {
-            var vloggersAndFollowers = new Dictionary<string, Dictionary<string, HashSet<string>>>();
+            //solution with one dictionary and nested hasSet
+            //var vloggersAndFollowers = new Dictionary<string, Dictionary<string, HashSet<string>>>();
+
+            //while (true)
+            //{
+            //    string input = Console.ReadLine();
+
+            //    if (input == "Statistics")
+            //    {
+            //        break;
+            //    }
+
+            //    string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            //    string vloggerName = tokens[0];
+            //    string action = tokens[1];
+
+            //    if (action == "joined")
+            //    {
+            //        if (vloggersAndFollowers.ContainsKey(vloggerName) == false)
+            //        {
+            //            vloggersAndFollowers.Add(vloggerName, new Dictionary<string, HashSet<string>>());
+            //            vloggersAndFollowers[vloggerName].Add("followers", new HashSet<string>());
+            //            vloggersAndFollowers[vloggerName].Add("following", new HashSet<string>());
+            //        }
+            //    }
+            //    else if (action == "followed")
+            //    {
+            //        string member = tokens[2];
+
+            //        if (vloggerName == member)
+            //        {
+            //            continue;
+            //        }
+
+            //        //•	"{vloggername} followed {vloggername}" 
+
+            //        if (vloggerName != member && vloggersAndFollowers.ContainsKey(vloggerName) && vloggersAndFollowers.ContainsKey(member))
+            //        {
+            //            vloggersAndFollowers[vloggerName]["following"].Add(member);
+            //            vloggersAndFollowers[member]["followers"].Add(vloggerName);
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine($"The V-Logger has a total of {vloggersAndFollowers.Keys.Count} vloggers in its logs.");
+
+            //int count = 1;
+
+            //var statistics = vloggersAndFollowers
+            //    .OrderByDescending(x => x.Value["followers"].Count)
+            //    .ThenBy(x => x.Value["following"].Count)
+            //    .ToList();
+
+            //foreach (var kvp in statistics)
+            //{
+            //    Console.WriteLine($"{count}. {kvp.Key} : {kvp.Value["followers"].Count} followers, {kvp.Value["following"].Count} following");
+            //    if (count == 1)
+            //    {
+            //        foreach (var item in kvp.Value["followers"].OrderBy(x => x))
+            //        {
+            //            Console.WriteLine($"*  {item}");
+            //        }
+            //    }
+            //    count++;
+            //}
+
+            //solution with two dictionary
+            var vloggersAndFollowers = new Dictionary<string, List<string>>();
+            var vloggersAndFollowing = new Dictionary<string, List<string>>();
 
             while (true)
             {
@@ -27,26 +95,28 @@ namespace _07._The_V_Logger
                 {
                     if (vloggersAndFollowers.ContainsKey(vloggerName) == false)
                     {
-                        vloggersAndFollowers.Add(vloggerName, new Dictionary<string, HashSet<string>>());
-                        vloggersAndFollowers[vloggerName].Add("followers", new HashSet<string>());
-                        vloggersAndFollowers[vloggerName].Add("following", new HashSet<string>());
+                        vloggersAndFollowers.Add(vloggerName, new List<string>());
+                        vloggersAndFollowing.Add(vloggerName, new List<string>());
                     }
                 }
                 else if (action == "followed")
                 {
-                    string member = tokens[2];
+                    string secondVloggerName = tokens[2];
 
-                    if (vloggerName == member)
+                    if (vloggerName == secondVloggerName)
                     {
                         continue;
                     }
 
                     //•	"{vloggername} followed {vloggername}" 
 
-                    if (vloggerName != member && vloggersAndFollowers.ContainsKey(vloggerName) && vloggersAndFollowers.ContainsKey(member))
+                    if (vloggersAndFollowers.ContainsKey(secondVloggerName) && vloggersAndFollowing.ContainsKey(vloggerName))
                     {
-                        vloggersAndFollowers[vloggerName]["following"].Add(member);
-                        vloggersAndFollowers[member]["followers"].Add(vloggerName);
+                        if (vloggersAndFollowers[secondVloggerName].Contains(vloggerName) == false)
+                        {
+                            vloggersAndFollowers[secondVloggerName].Add(vloggerName);
+                            vloggersAndFollowing[vloggerName].Add(secondVloggerName);
+                        }
                     }
                 }
             }
@@ -56,94 +126,25 @@ namespace _07._The_V_Logger
             int count = 1;
 
             var statistics = vloggersAndFollowers
-                .OrderByDescending(x => x.Value["followers"].Count)
-                .ThenBy(x => x.Value["following"].Count)
+                .OrderByDescending(x => x.Value.Count())
+                .ThenBy(x => vloggersAndFollowing[x.Key].Count())
                 .ToList();
+
+
 
             foreach (var kvp in statistics)
             {
-                Console.WriteLine($"{count}. {kvp.Key} : {kvp.Value["followers"].Count} followers, {kvp.Value["following"].Count} following");
+                int followingCount = vloggersAndFollowing[kvp.Key].Count;
+                Console.WriteLine($"{count}. {kvp.Key} : {kvp.Value.Count} followers, {followingCount} following");
                 if (count == 1)
                 {
-                    foreach (var item in kvp.Value["followers"].OrderBy(x => x))
+                    foreach (var item in kvp.Value.OrderBy(x => x))
                     {
                         Console.WriteLine($"*  {item}");
                     }
                 }
                 count++;
             }
-
-            //30/100 => incorrect sorting
-            //var vloggersAndFollowers = new Dictionary<string, List<string>>();
-            //var vloggersAndFollowing = new Dictionary<string, List<string>>();
-
-            //while (true)
-            //{
-            //    string input = Console.ReadLine();
-
-            //    if(input == "Statistics")
-            //    {
-            //        break;
-            //    }
-
-            //    string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            //    string vloggerName = tokens[0];
-            //    string action = tokens[1];
-
-            //    if(action == "joined")
-            //    {
-            //        if(vloggersAndFollowers.ContainsKey(vloggerName) == false)
-            //        {
-            //            vloggersAndFollowers.Add(vloggerName, new List<string>());
-            //            vloggersAndFollowing.Add(vloggerName, new List<string>());
-            //        }
-            //    }
-            //    else if(action == "followed")
-            //    {
-            //        string secondVloggerName = tokens[2];
-
-            //        if (vloggerName == secondVloggerName)
-            //        {
-            //            continue;
-            //        }
-
-            //        //•	"{vloggername} followed {vloggername}" 
-
-            //        if (vloggersAndFollowers.ContainsKey(secondVloggerName) && vloggersAndFollowing.ContainsKey(vloggerName))
-            //        {
-            //            if(vloggersAndFollowers[secondVloggerName].Contains(vloggerName) == false)
-            //            {
-            //                vloggersAndFollowers[secondVloggerName].Add(vloggerName);
-            //                vloggersAndFollowing[vloggerName].Add(secondVloggerName);
-            //            }
-            //        }                   
-            //    }
-            //}
-
-            //Console.WriteLine($"The V-Logger has a total of {vloggersAndFollowers.Keys.Count} vloggers in its logs.");
-
-            //int count = 1;
-
-            //var statistics = vloggersAndFollowers
-            //    .OrderByDescending(x => x.Value.Count)
-            //    .ThenBy(x=>vloggersAndFollowing.Keys.Count)
-            //    .ToList();
-
-
-
-            //foreach (var kvp in statistics)
-            //{
-            //    int followingCount = vloggersAndFollowing[kvp.Key].Count;
-            //    Console.WriteLine($"{count}. {kvp.Key} : {kvp.Value.Count} followers, {followingCount} following");
-            //    if(count == 1)
-            //    {
-            //        foreach (var item in kvp.Value.OrderBy(x=>x))
-            //        {
-            //            Console.WriteLine($"*  {item}");
-            //        }
-            //    }
-            //    count++;
-            //}
         }
     }
 }
