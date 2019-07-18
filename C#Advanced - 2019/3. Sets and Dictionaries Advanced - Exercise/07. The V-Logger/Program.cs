90f7538aff0de7a6049858e10d2d7bd16d71535b
Elvira <elvira.lyubomirova.petkova@abv.bg>
2019-07-18 10:34:23 +0300

    Add solutions in 3.Sets and Dictionaries Advanced

---
+++ C#Advanced - 2019/3. Sets and Dictionaries Advanced - Exercise/07. The V-Logger/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._The_V_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vloggersAndFollowers = new Dictionary<string, Dictionary<string, HashSet<string>>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Statistics")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string vloggerName = tokens[0];
+                string action = tokens[1];
+
+                if (action == "joined")
+                {
+                    if (vloggersAndFollowers.ContainsKey(vloggerName) == false)
+                    {
+                        vloggersAndFollowers.Add(vloggerName, new Dictionary<string, HashSet<string>>());
+                        vloggersAndFollowers[vloggerName].Add("followers", new HashSet<string>());
+                        vloggersAndFollowers[vloggerName].Add("following", new HashSet<string>());
+                    }
+                }
+                else if (action == "followed")
+                {
+                    string member = tokens[2];
+
+                    if (vloggerName == member)
+                    {
+                        continue;
+                    }
+
+                    //•	"{vloggername} followed {vloggername}" 
+
+                    if (vloggerName != member && vloggersAndFollowers.ContainsKey(vloggerName) && vloggersAndFollowers.ContainsKey(member))
+                    {
+                        vloggersAndFollowers[vloggerName]["following"].Add(member);
+                        vloggersAndFollowers[member]["followers"].Add(vloggerName);
+                    }
+                }
+            }
+
+            Console.WriteLine($"The V-Logger has a total of {vloggersAndFollowers.Keys.Count} vloggers in its logs.");
+
+            int count = 1;
+
+            var statistics = vloggersAndFollowers
+                .OrderByDescending(x => x.Value["followers"].Count)
+                .ThenBy(x => x.Value["following"].Count)
+                .ToList();
+
+            foreach (var kvp in statistics)
+            {
+                Console.WriteLine($"{count}. {kvp.Key} : {kvp.Value["followers"].Count} followers, {kvp.Value["following"].Count} following");
+                if (count == 1)
+                {
+                    foreach (var item in kvp.Value["followers"].OrderBy(x => x))
+                    {
+                        Console.WriteLine($"*  {item}");
+                    }
+                }
+                count++;
+            }
+
+            //30/100 => incorrect sorting
+            //var vloggersAndFollowers = new Dictionary<string, List<string>>();
+            //var vloggersAndFollowing = new Dictionary<string, List<string>>();
+
+            //while (true)
+            //{
+            //    string input = Console.ReadLine();
+
+            //    if(input == "Statistics")
+            //    {
+            //        break;
+            //    }
+
+            //    string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            //    string vloggerName = tokens[0];
+            //    string action = tokens[1];
+
+            //    if(action == "joined")
+            //    {
+            //        if(vloggersAndFollowers.ContainsKey(vloggerName) == false)
+            //        {
+            //            vloggersAndFollowers.Add(vloggerName, new List<string>());
+            //            vloggersAndFollowing.Add(vloggerName, new List<string>());
+            //        }
+            //    }
+            //    else if(action == "followed")
+            //    {
+            //        string secondVloggerName = tokens[2];
+
+            //        if (vloggerName == secondVloggerName)
+            //        {
+            //            continue;
+            //        }
+
+            //        //•	"{vloggername} followed {vloggername}" 
+
+            //        if (vloggersAndFollowers.ContainsKey(secondVloggerName) && vloggersAndFollowing.ContainsKey(vloggerName))
+            //        {
+            //            if(vloggersAndFollowers[secondVloggerName].Contains(vloggerName) == false)
+            //            {
+            //                vloggersAndFollowers[secondVloggerName].Add(vloggerName);
+            //                vloggersAndFollowing[vloggerName].Add(secondVloggerName);
+            //            }
+            //        }                   
+            //    }
+            //}
+
+            //Console.WriteLine($"The V-Logger has a total of {vloggersAndFollowers.Keys.Count} vloggers in its logs.");
+
+            //int count = 1;
+
+            //var statistics = vloggersAndFollowers
+            //    .OrderByDescending(x => x.Value.Count)
+            //    .ThenBy(x=>vloggersAndFollowing.Keys.Count)
+            //    .ToList();
+
+
+
+            //foreach (var kvp in statistics)
+            //{
+            //    int followingCount = vloggersAndFollowing[kvp.Key].Count;
+            //    Console.WriteLine($"{count}. {kvp.Key} : {kvp.Value.Count} followers, {followingCount} following");
+            //    if(count == 1)
+            //    {
+            //        foreach (var item in kvp.Value.OrderBy(x=>x))
+            //        {
+            //            Console.WriteLine($"*  {item}");
+            //        }
+            //    }
+            //    count++;
+            //}
+        }
+    }
+}
---
+++ C#Advanced - 2019/3. Sets and Dictionaries Advanced - Exercise/08. Ranking/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Ranking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataOfCourses = new Dictionary<string, string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if(input == "end of contests")
+                {
+                    break;
+                }
+
+                string[] toknes = input.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                string contest = toknes[0];
+                string password = toknes[1];
+
+                dataOfCourses.Add(contest, password); //not cheking key contest
+            }
+
+            var users = new Dictionary<string, Dictionary<string, int>>();
+            //key=userName, key = contest, value = points
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if(input == "end of submissions")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split("=>"); //"{contest}=>{password}=>{username}=>{points}" 
+                string contest = tokens[0];
+                string password = tokens[1];
+                string userName = tokens[2];
+                int points = int.Parse(tokens[3]);
+
+                if (dataOfCourses.ContainsKey(contest))
+                {
+                    if(dataOfCourses[contest] == password)
+                    {
+                        if(users.ContainsKey(userName) == false)
+                        {
+                            users.Add(userName, new Dictionary<string, int>());
+                        }
+
+                        if(users[userName].ContainsKey(contest) == false)
+                        {
+                            users[userName].Add(contest, points);
+                        }
+                        else
+                        {
+                            if(users[userName][contest] < points)
+                            {
+                                users[userName][contest] = points;
+                            }
+                        }
+                    }
+                }
+            }
+
+            KeyValuePair<string, int> bestUser = BestUser(users);
+            Console.WriteLine($"Best candidate is {bestUser.Key} with total {bestUser.Value} points.");
+            Console.WriteLine("Ranking:");
+
+            foreach (var kvp in users.OrderBy(x=>x.Key))
+            {
+                Console.WriteLine(kvp.Key);
+
+                foreach (var contest in kvp.Value.OrderByDescending(x=>x.Value))
+                {
+                    Console.WriteLine($"#  {contest.Key} -> {contest.Value}");
+                }
+            }
+        }
+
+        private static KeyValuePair<string, int> BestUser(Dictionary<string, Dictionary<string, int>> users)
+        {
+            int bestSum = 0;
+            string userOfBest = string.Empty;
+
+            foreach (var kvp in users)
+            {
+                string userName = kvp.Key;
+                int sumOfPoints = kvp.Value.Values.Sum();
+
+                if (bestSum < sumOfPoints)
+                {
+                    bestSum = sumOfPoints;
+                    userOfBest = userName;
+                }
+            }
+
+            KeyValuePair<string, int> bestUser = new KeyValuePair<string, int>(userOfBest, bestSum);
+            return bestUser;
+        }
+    }
+}
