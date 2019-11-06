d1bf57c4f75d9c0001a9110f6d27bd8fca56aa01
ElviraPetkova <elvira.lyubomirova.petkova@abv.bg>
2019-11-06 16:51:58 +0200

    add solution

---
+++ C# OOP - 2019/InterfacesAndAbstraction/ExplicitInterfaces/Models/Citizen.cs
@@ -0,0 +1,30 @@
+﻿namespace ExplicitInterfaces.Models
+{
+    using Contracts;
+
+    public class Citizen : IResident, IPerson
+    {
+        public Citizen()
+        {              
+        }
+
+        public Citizen(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; }
+
+        public string Country { get; }
+
+        public int Age { get; }
+
+        string IResident.GetName()
+            => $"Mr/Ms/Mrs {this.Name}";
+
+        string IPerson.GetName()
+            => $"{this.Name}";
+
+        
+    }
+}
---
+++ C# OOP - 2019/InterfacesAndAbstraction/ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace ExplicitInterfaces
+{
+    using System;
+    using Contracts;
+    using Models;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] arguments = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                //name, country, age
+                Citizen citizen = new Citizen(arguments[0]);
+                IResident rezident = citizen;
+                IPerson person = citizen;
+
+                Console.WriteLine($"{person.GetName()}");
+                Console.WriteLine($"{rezident.GetName()}");
+            }
+        }
+    }
+}
